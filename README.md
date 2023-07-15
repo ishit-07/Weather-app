be2b124e26cbef55580be4c82ace1216d4e38005
Ishit Verma <ishitverma2602@gmail.com>
2023-07-15 22:50:44 +0530

    version 1 :rocket:

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# Weather-app
+> **Note**: 🚧 This project is Work-In-progres
+
+# WeatherApp
+
+WeatherApp is a simple and intuitive weather application that provides current weather information for any city in the world. It utilizes the OpenWeather API to fetch real-time weather data and presents it in a user-friendly manner.
+
+## Key Features
+
+- **Current Weather**: Get instant access to the current weather conditions of any city.
+- **City Search**: Easily search for a specific city and receive accurate weather data.
+- **Temperature**: View the temperature in Celsius or Fahrenheit, based on your preference.
+- **Weather Description**: Get a brief description of the weather conditions, such as sunny, cloudy, or rainy.
+- **Additional Information**: Access more detailed information about the weather, including humidity, wind speed, and atmospheric pressure.
+
+## Getting Started
+
+To use the WeatherApp, you need to obtain an API key from OpenWeather. Follow the steps below to set up your API key and run the application:
+
+1. Clone the WeatherApp repository to your local machine.
+
+```bash
+git clone https://github.com/your-username/WeatherApp.git
+```
+
+2. Sign up for a free account on the OpenWeather website at [https://openweathermap.org/](https://openweathermap.org/).
+
+3. Once you have signed up and logged in, navigate to your account dashboard and locate your API key.
+
+4. Copy the API key and open the `script.js` file in the WeatherApp project directory.
+
+5. Paste your API key into the `API_KEY` variable and save the file.
+
+```javascript
+// script.js
+const API_KEY = "<OPEN_WEATHER_API_KEY>";
+```
+
+:rocket: The WeatherApp will now be running on your local machine. Open your web browser and access it at [http://localhost:5500](http://localhost:5500).
+
+## Usage
+
+1. Upon launching the WeatherApp, you will be greeted with a simple and elegant user interface.
+
+2. Enter the name of the city you want to get weather information for in the search bar.
+
+3. Press the "Search" button or hit Enter to fetch the weather data.
+
+4. The current weather conditions for the specified city will be displayed, including temperature, weather description, humidity, wind speed, and atmospheric pressure.
+
+5. You can toggle between Celsius and Fahrenheit by clicking on the temperature unit button.
+
+6. To search for weather information for another city, simply enter the name of the new city and repeat steps 3-5.
+
+### Example
+
+Enter a city name: Pune
+
+Current weather in Pune:
+
+Temperature: 23.1°C
+Humidity: 85%
+Wind: 15.8 km/h
+Chance of rain: 60%
+
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- OpenWeather API
+## Acknowledgements
+
+- The WeatherApp utilizes the [OpenWeather API](https://openweathermap.org/api) to fetch real-time weather data.
+- The user interface design is inspired by various weather applications and modern UI trends.
+
+
---
+++ script.js
@@ -0,0 +1,118 @@
+const API_KEY = "<OPEN_WEATHER_API_KEY>";
+
+
+// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+let searchDetail = document.querySelector(".search input");
+let formSumbmit = document.querySelector("form");
+
+let cityName = document.querySelector(".cities h1");
+let chanceRain = document.querySelector(".cities small");
+let temperature = document.querySelector(".cities h2");
+let weatherImage = document.querySelector(".cities img");
+
+// update air conditions details
+
+let realFeel = document.querySelector(".real-feel h1");
+let wind = document.querySelector(".wind h1");
+let chanceOfRain = document.querySelector(".chance-of-rain h1");
+let UVIndex = document.querySelector(".uv-index h1");
+let humidity = document.querySelector(".humidity h1");
+
+formSumbmit.addEventListener("submit", search);
+
+let target;
+
+const fetchData = async (targetLocation) => {
+  let url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${targetLocation}&aqi=no`;
+
+  const response = await fetch(url);
+
+  const data = await response.json();
+
+  // console.log(data);
+
+  // Main data
+  let locationNameData = data.location.name;
+  let weatherImageData = data.current.condition.icon;
+  let cloudData = data.current.cloud;
+
+  // data of Air conditions
+  let degree_C_Data = data.current.temp_c;
+  let windSpeedData = data.current.wind_kph;
+  let UVIndexData = data.current.uv;
+  let realFeelData = data.current.feelslike_c;
+  let humidityData = data.current.humidity;
+
+  updataDetails(
+    locationNameData,
+    weatherImageData,
+    cloudData,
+    degree_C_Data,
+    windSpeedData,
+    UVIndexData,
+    realFeelData,
+    humidityData
+  );
+
+  // console.log(weatherImageData);
+};
+
+function updataDetails(
+  locationNameData,
+  weatherImageData,
+  cloudData,
+  degree_C_Data,
+  windSpeedData,
+  UVIndexData,
+  realFeelData,
+  humidityData
+) {
+  cityName.innerText = locationNameData;
+  chanceRain.innerText = `Chance of rain: ${cloudData}%`;
+  weatherImage.src = `${weatherImageData}`;
+  temperature.innerText = `${degree_C_Data}°C`;
+
+  realFeel.innerText = `${realFeelData}°C`;
+  wind.innerText = `${windSpeedData} km/h`;
+  chanceOfRain.innerText = `${cloudData}%`;
+  UVIndex.innerText = UVIndexData;
+  humidity.innerText = `${humidityData}%`;
+}
+
+function search(e) {
+  e.preventDefault();
+
+  target = searchDetail.value;
+  fetchData(target);
+}
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "January",
+  "Feburary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const time = new Date();
+const date = time.getDay();
+const month = time.getMonth();
+console.log(days[date]);
+console.log(months[month]);
